f790ad7823e4f2686c2a8d9f176610360cd049be
shafnybuilds <shafnymhd@proton.me>
2025-04-04 00:38:47 +0530

    columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import TaskCard from "@/components/task-card";
-import { tasks } from "@/utils/data-tasks";
+import { tasks, statuses } from "@/utils/data-tasks";
 
 function Home() {
+  const columns = statuses.map((status) => {
+    const tasksInColumn = tasks.filter((task) => task.status === status);
+    return {
+      title: status,
+      tasks: tasksInColumn,
+    };
+  });
+
   return (
     <>
-      {tasks.map((task) => {
-        return <TaskCard task={task} />;
-      })}
+      <div className="flex divide-y">
+        {columns.map((column) => (
+          <div>
+            <h1>{column.title}</h1>
+            {column.tasks.map((task) => (
+              <TaskCard task={task} />
+            ))}
+          </div>
+        ))}
+      </div>
+
       {/* <TaskCard task={task} /> */}
       {/* <TaskCard title={"Competitor Analysis"} id={"BUS 2"} points={8} />
       <TaskCard title={"Develop Business Stratgy"} id={"BUS 3"} /> */}
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,28 +1,61 @@
+export type Status = "to-do" | "in-progress" | "done";
+
 export type Task = {
   title: string;
   id: string;
+  status: Status;
   points?: number;
 };
+
+export const statuses: Status[] = ["to-do", "in-progress", "done"];
 
 export const tasks: Task[] = [
   // we can also say Array<Task>
   {
     title: "Do Market Research",
     id: "BUS 1",
+    status: "to-do",
     points: 5,
   },
   {
     title: "Competitor Analysis",
     id: "BUS 2",
+    status: "in-progress",
   },
   {
     title: "Develop a Business Strategy",
     id: "BUS 3",
+    status: "to-do",
     points: 8,
   },
   {
     title: "Develop Marketing Strategy",
     id: "BUS 4",
+    status: "in-progress",
     points: 5,
+  },
+  {
+    title: "Negotiate Partnerships",
+    id: "BUS 5",
+    status: "to-do",
+    points: 3,
+  },
+  {
+    title: "Develop marketing plan",
+    id: "BUS 6",
+    status: "to-do",
+    points: 5,
+  },
+  {
+    title: "Implement marketing plan",
+    id: "BUS 7",
+    status: "in-progress",
+    points: 8,
+  },
+  {
+    title: "Evaluate Business Performance",
+    id: "BUS 8",
+    status: "done",
+    points: 6,
   },
 ];
